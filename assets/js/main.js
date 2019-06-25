ceb1c8a5aa86397ee40e0a24fb50176da137d602
danmtt <37252746+danmtt@users.noreply.github.com>
2019-06-25 07:32:22 +0100

    Folder Structure Update: Split JS files into different ones to make easier work with functionalities.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
-// Define variables to identify cluster buttons.
-var hotelMarker = getElementById('hotel-marker');
-var foodMarker = getElementById('food-marker');
-var pubMarker = getElementById('pub-marker');
-var musicMarker = getElementById('music-marker');
-var artsMarker = getElementById('arts-marker');
-var sportsMarker = getElementById('sports-marker');
-var outingMarker = getElementById('outing-marker');
-var relaxMarker = getElementById('relax-marker');
+
 
 // Map functionality.
 function initMap() {
@@ -158,30 +150,7 @@ function initMap() {
   });
 };
 
-    // Hotel markers
-    // https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
 
-
-    hotelMarker.addListener("click", function() {
-      map.setZoom(3);
-      // window.alert("Hotels have been selected for");
-    });
-
-
-
-
-// Cluster functionality
-
-// Add event listeners to every cluster in the modal form so when clicked ,
-// markers related are updated to map.
-
-// function onClickClusterButton(){
-//   var lookFor = {
-//     bounds: map.getBounds(),
-//     types: ["lodging"]
-//   };
-//   window.alert("Hotels have been selected for : '" + place.name + "'");
-// };
 
 
 
