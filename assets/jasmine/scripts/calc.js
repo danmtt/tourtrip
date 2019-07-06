9573db75440326276e9fb9006d80afd45d7966e8
danmtt <37252746+danmtt@users.noreply.github.com>
2019-07-06 23:10:29 +0100

    JASMINE Update: Test draft optimizing

--- assets/jasmine/scripts/calc.js
+++ assets/jasmine/scripts/calc.js
@@ -1,1 +1,83 @@
 
+// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+let button4 = document.querySelector('#button4');
+let log = document.querySelector('#log');
+button1.addEventListener('mouseup', logMouseButton1);
+button2.addEventListener('mouseup', logMouseButton2);
+button3.addEventListener('mouseup', logMouseButton3);
+button4.addEventListener('mouseup', logMouseButton4);
+
+function logMouseButton1(e) {
+    if (typeof e === 'object') {
+        switch (e.button) {
+        case 0:
+            var onClickButton ='lodging';
+            log.textContent = 'Button clicked ID:'+ this.id + 'Value' + onClickButton;
+
+            break;
+        case 1:
+            log.textContent = 'Middle button clicked.';
+            break;
+        case 2:
+            log.textContent = 'Right button clicked.';
+            break;
+        default:
+            log.textContent = `Unknown button code: ${btnCode}`;
+        }
+  }
+}
+function logMouseButton2(e) {
+    if (typeof e === 'object') {
+        switch (e.button) {
+        case 0:
+            log.textContent = 'Button clicked ID:'+ this.id + 'Left button clicked.';
+            break;
+        case 1:
+            log.textContent = 'Middle button clicked.';
+            break;
+        case 2:
+            log.textContent = 'Right button clicked.';
+            break;
+        default:
+            log.textContent = `Unknown button code: ${btnCode}`;
+        }
+    }
+}
+function logMouseButton3(e) {
+    if (typeof e === 'object') {
+        switch (e.button) {
+        case 0:
+            log.textContent = 'Button clicked ID:'+ this.id + 'Left button clicked.';
+            break;
+        case 1:
+            log.textContent = 'Middle button clicked.';
+            break;
+        case 2:
+            log.textContent = 'Right button clicked.';
+            break;
+        default:
+            log.textContent = `Unknown button code: ${btnCode}`;
+        }
+    }
+}
+function logMouseButton4(e) {
+    if (typeof e === 'object') {
+        switch (e.button) {
+        case 0:
+            log.textContent = 'Button clicked ID:'+ this.id + 'Left button clicked.';
+            break;
+        case 1:
+            log.textContent = 'Middle button clicked.';
+            break;
+        case 2:
+            log.textContent = 'Right button clicked.';
+            break;
+        default:
+            log.textContent = `Unknown button code: ${btnCode}`;
+        }
+    }
+}
--- assets/jasmine/specs/calcSpec.js
+++ assets/jasmine/specs/calcSpec.js
@@ -1,6 +1,33 @@
-// I want to change the value of a variable when clicking on an specific html element.
-// To do that I have created a div with several buttons on it with different
-// id´s. Everytime a button is clicked, the value of the variable changes and
-// an alert is displayed showing the id of the button clicked and the new value assigned
+// TEST 01
+// I want to test the button clicking functionality
+//      I am going to test if clicking is true
+//      I want to get the result of the variable original value.
 
-var onClickButton=''
+// https://stackoverflow.com/questions/16890618/test-for-click-event-using-jasmine-test-suite
+// describe('view interactions', function () {
+//     beforeEach(function () {
+//         this.clickEventStub = sinon.stub(this, 'clickEvent');
+//     });
+
+//     afterEach(function () {
+//         this.clickEvent.restore();
+//     });
+
+//     describe('when item is clicked', function () {
+//         it('event is fired', function () {
+//             this.elem.trigger('click');
+//             expect(this.clickEventStub).toHaveBeenCalled();
+//         });
+//     });
+// });
+
+// https://devhints.io/jasmine
+// https://codereview.stackexchange.com/questions/32573/spyon-click-events-and-check-call-function
+// describe('Button clicking functionality', function () {
+//     describe('test if clicking is true', function () {        
+//         it ('spy on behavior', function () {
+//             spy1= spyOnEvent($('button1'), 'click');
+//             expect(spy1).toHaveBeenCalled();
+//         })
+//     })
+// })
