2cad5a964ec1c26e8f0d09d0139389c49f9205ee
danmtt <37252746+danmtt@users.noreply.github.com>
2019-07-06 20:57:14 +0100

    JASMINE Update: Folder structure optimizing and TEST 01 draft

---
+++ assets/jasmine/specs/calcSpec.js
@@ -0,0 +1,6 @@
+// I want to change the value of a variable when clicking on an specific html element.
+// To do that I have created a div with several buttons on it with different
+// id´s. Everytime a button is clicked, the value of the variable changes and
+// an alert is displayed showing the id of the button clicked and the new value assigned
+
+var onClickButton=''
