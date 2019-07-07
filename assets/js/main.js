a23a86cfb23381b509cddedc78030943ad8590a3
danmtt <37252746+danmtt@users.noreply.github.com>
2019-07-08 00:23:05 +0100

    JS Update: Service.nearbySearch optimizing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,22 @@
 
-
-let map, autocomplete, infowindow, service; // objects
+// Global variables
+var map, autocomplete, infowindow, service; // OBJECTS
 var mapCanvas, mapOptions; // map variables
 var contentString; // Infowindow content
-var markerMapOptions; // Main marker in map options(place)
+var markerMapOptions; // To set the style of the main marker in map (place)
 var place,bounds; 
 
 // Map function.
 function initMap() {
-  // VARIABLES UPDATE
+
+  // OBJECTS UPDATE
+  // --------------
+
+  // MAP Object -----------------------------------------------------------------------------------
   // To define into a variable in which HTML element the map should be diplayed.
   var mapCanvas = document.getElementById("map");
   // To define into a variable the options to use with the map.  
-  var mapOptions={
+  var mapOptions = {
     disableDefaultUI: true,
     mapTypeId: 'satellite',
     // to hide all labels from map
@@ -37,18 +41,14 @@ function initMap() {
     ]
     
   };
-  // To define into a variable the infowindow structure, storing inside an HTML Snippet in which to release the content retrieved
-  var contentString =
-  '<div id="infowindow-content" class="d-flex flex-column justify-content-center ">'+
-    '<h1 id="infowindow-heading" class="justify-content-center text-center"></h1>'+
-    '<div id="bodyContent">'+
-      '<div id="infowindow-description" class="text-center">Your destination is in <b><span id="place-type">(country)</span></b></div>'+
-      '<div id="infowindow-image" class="justify-content-center img-responsive center-block"></div>' +
-    '</div>'+
-  '</div>';
+  // To create the 'map' object, updating the values defined in a variable.
+  // https://developers.google.com/maps/documentation/javascript/examples/map-simple
+  map = new google.maps.Map(mapCanvas, mapOptions);
+   
+  // MARKER Object  -------------------------------------------------------------------------------
   // To define into a variable the options selected to modify the marker vehabiour an its style.
   // https://developers.google.com/maps/documentation/javascript/examples/marker-animations 
-  var markerMapOptions = {
+  var markerOptions = {
     animation: google.maps.Animation.BOUNCE,
     label: "Your destination",
     // content: markerLabel,
@@ -56,39 +56,43 @@ function initMap() {
     title: "Click to zoom"
     // https://developers.google.com/maps/documentation/javascript/examples/overlay-popup
     // icon: iconImage + 'parking_lot_maps.png',
-   };
-  // To define which HTML element is the input search box, setting that info in a variable.
-  var searchInput = document.getElementById("search-input");
-
-  // To define the search options to use with autocomplete, restricting the search to cities. This is to avoid street searches...
-  var searchOptions={
-    type: ['(cities)'],
   };
-
-  // OBJECTS UPDATE
-  // --------------
-  
-  // To create the 'map' object, updating the values defined in a variable.
-  // https://developers.google.com/maps/documentation/javascript/examples/map-simple
-  map = new google.maps.Map(mapCanvas, mapOptions);
-   
-  // To create the 'infowindow' object, setting the content retrieved using a variable previously defined.
-  // https://developers.google.com/maps/documentation/javascript/infowindows
-  infowindow = new google.maps.InfoWindow({content: contentString});
-
   // To create the 'marker' object, setting the options usig a variable previously defined.
   // https://developers.google.com/maps/documentation/javascript/markers
-  marker = new google.maps.Marker(markerMapOptions)
-  // To add the marker created into the map.
-  marker.setMap(map);
+  marker = new google.maps.Marker(markerOptions)
+  
+  // INFOWINDOW Object  ---------------------------------------------------------------------------
+  // To define into a variable the infowindow structure, storing inside an HTML Snippet 
+  // in which to release the content retrieved
+  contentString =
+  '<div id="infowindow-content" class="d-flex flex-column justify-content-center ">'+
+    '<h1 id="infowindow-heading" class="justify-content-center text-center"></h1>'+
+    '<div id="bodyContent">'+
+      '<div id="infowindow-description" class="text-center">Your destination is in <b><span id="place-type">(country)</span></b></div>'+
+      '<div id="infowindow-image" class="justify-content-center img-responsive center-block"></div>' +
+    '</div>'+
+  '</div>';
+  // To create the 'infowindow' object, setting the content retrieved using a variable 
+  // previously defined.
+  // https://developers.google.com/maps/documentation/javascript/infowindows
+  infowindow = new google.maps.InfoWindow({content: contentString});
  
+  // AUTOCOMPLETE Object  -------------------------------------------------------------------------
+  // To define which HTML element is the input search box, setting that info in a variable.
+  var searchInput = document.getElementById("search-input");
+  // To define the search options to use with autocomplete, restricting the search to cities. 
+  // This is to avoid street searches...
+  var searchOptions = {
+    type: ['(cities)'],
+  };
   // To create the 'autocomplete' object, updating the values defined in two variables.
   // https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete
   autocomplete = new google.maps.places.Autocomplete(searchInput, searchOptions);
 
   // To set 'bounds' values into map created.
   autocomplete.bindTo('bounds', map); 
-  
+   
+  // SERVICE Object  ----------------------------------------------------------------------------- 
   // To create the 'service' object, relating the information to map created.
   //https://developers.google.com/maps/documentation/javascript/examples/place-search-pagination
   service = new google.maps.places.PlacesService(map); 
@@ -97,10 +101,14 @@ function initMap() {
   // that will run a custom callback function in response to the media query status changing.
   autocomplete.addListener('place_changed', function() {
     infowindow.close();
+    
+    // To add the marker created into the map.
+    marker.setMap(map);
 
     var place = autocomplete.getPlace();
-
+    // To add the name of the place searched into one of the HTML modal's elements
     document.getElementById('destination').innerHTML = place.name;
+
     
     // This condition checks if the name of a Place introduced
     // by the user after pressing the Enter key (not the Search button),
@@ -129,10 +137,6 @@ function initMap() {
  
     marker.addListener('click', function() {
 
-      infowindow.open(map, marker);
-      infowindow.addListener('close',function(){
-        map.setCenter(place.geometry.location);
-      });
       // To stop the marker bouncing when clicking on it.
       // https://developers.google.com/maps/documentation/javascript/examples/marker-animations
       marker.setAnimation(google.maps.Animation.DROP);
@@ -143,12 +147,11 @@ function initMap() {
       // https://ourcodeworld.com/articles/read/831/how-to-change-and-preview-map-type-in-google-maps-dinamically-with-javascript
       map.setZoom(15); // Zoom
       map.setMapTypeId("roadmap"); // TypeId
-      // map.setMapTypeId(google.maps.MapTypeId.ROADMAP); 
-      // map.setOptions()
       
+      infowindow.open(map, marker);
       // To set the values retrieved from the calllback function to different HTML elements in the modal form.      
-      document.getElementById('infowindow-heading').innerHTML = place.name;
-    
+      document.getElementById('infowindow-heading').innerHTML = place.name; 
+
       // To select the first image available using a call back function as method to another previous function
       // and store it into a variable.
       var placeImg = place.photos[0].getUrl({maxWidth: 300, maxHeight: 300});
@@ -167,20 +170,53 @@ function initMap() {
       }   
     });
 
-    // To set an event listener when clicking the marker, opening the info window
-    // updating information about the place selected
-    // and zooming into the location.
-    google.maps.event.addListener(map, 'click', function() {
+    // infowindow.addListener('close',function(){
+    //   map.setCenter(place.geometry.location);
+    // });
+
+    // To set an event listener to 'infowindow' when closing it,
+    // the map is centered to the marker position  
+    
+    // infowindow.addListener('click', function() {
+    //   infowindow.close();
+    //   map.setCenter(place.geometry.location);
+    // });
+    
+    // To set an event listener to 'map' when clicking out of infowindow,
+    // this one dissapears and the map is centered to the marker position  
+    
+    map.addListener('click', function() {
       infowindow.close();
       map.setCenter(place.geometry.location);
     });
     
+ 
+  // display onClickClusterButton into an html element
+   
+  var onClickClusterButton = '';
+
+  // Set variables for specific buttons id´s to check onClick events
+  let hotelMarkers = document.querySelector('#hotel-markers');
+  let foodMarkers = document.querySelector('#food-markers');
+  let pubMarkers = document.querySelector('#pub-markers');
+  let atmMarkers = document.querySelector('#atm-markers');
+  let museumMarkers = document.querySelector('#museum-markers');
+  let galleryMarkers = document.querySelector('#gallery-markers');
+  let zooMarkers = document.querySelector('#zoo-markers');
+  let stadiumMarkers = document.querySelector('#stadium-markers');
+  let busMarkers = document.querySelector('#bus-markers');
+  let subwayMarkers = document.querySelector('#subway-markers');
+  let taxiMarkers = document.querySelector('#taxi-markers');
+  let airportMarkers = document.querySelector('#airport-markers');
+
+ 
+
   // Perform a nearby search.
   // https://developers.google.com/maps/documentation/javascript/places
 
   // service.nearbySearch( {location: place.geometry.location, radius: 500, type: ['lodging']},
-  service.nearbySearch( {location: place.geometry.location, radius: 500, type: onClickClusterButton},
-   
+  service.nearbySearch({location: place.geometry.location, radius: 500, type: onClickClusterButton},
+ 
   function(results, status, pagination) {
     if (status !== 'OK') return;
 
@@ -201,21 +237,26 @@ function initMap() {
       moreButton.disabled = true;
       if (getNextPage) getNextPage();
     }; 
-      
-
   
   function createMarkers(places) {
+    
+    hotelMarkers.onclick = function() {
+      onClickClusterButton ='lodging';
+    };
+    // service = new google.maps.places.PlacesService(map); 
+    // service.nearbySearch({location: place.geometry.location, radius: 500, type: onClickClusterButton},)
+
     var bounds = new google.maps.LatLngBounds();
     var serviceList = document.getElementById('map-displayed-markers');
 
     for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+      // var image = {
+      //   url: place.icon,
+      //   size: new google.maps.Size(71, 71),
+      //   origin: new google.maps.Point(0, 0),
+      //   anchor: new google.maps.Point(17, 34),
+      //   scaledSize: new google.maps.Size(25, 25)
+      // };
 
       // var marker = new google.maps.Marker({
       //   map: map,
@@ -233,18 +274,15 @@ function initMap() {
     }
     map.fitBounds(bounds);    
   }
-
-
-
 }); 
-
+  
 // NEXT STEP 
 
 // To assign the right value to a variable on different clicks
 // https://stackoverflow.com/questions/7132547/set-a-variable-value-on-click-display-the-value
 
 
-var onClickClusterButton = ''; // or add a default in the switch
+
 
 // $("input[type='button']").click(function() {
 
@@ -275,56 +313,53 @@ var onClickClusterButton = ''; // or add a default in the switch
 // https://stackoverflow.com/questions/2788191/how-to-check-whether-a-button-is-clicked-by-using-javascript
 // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
 
-let hotelMarkers = document.querySelector('#hotel-markers');
-let foodMarkers = document.querySelector('#food-markers');
-let pubMarkers = document.querySelector('#pub-markers');
-let atmMarkers = document.querySelector('#atm-markers');
-let museumMarkers = document.querySelector('#museum-markers');
-let galleryMarkers = document.querySelector('#gallery-markers');
-let zooMarkers = document.querySelector('#zoo-markers');
-let stadiumMarkers = document.querySelector('#stadium-markers');
-let busMarkers = document.querySelector('#bus-markers');
-let subwayMarkers = document.querySelector('#subway-markers');
-let taxiMarkers = document.querySelector('#taxi-markers');
-let airportMarkers = document.querySelector('#airport-markers');
 
-hotelMarkers.addEventListener('mouseup', logMouseHotelMarkers);
-foodMarkers.addEventListener('mouseup', logMouseFoodMarkers);
-pubMarkers.addEventListener('mouseup', logMousePubMarkers);
-atmMarkers.addEventListener('mouseup', logMouseAtmMarkers);
-museumMarkers.addEventListener('mouseup', logMouseMuseumMarkers);
-galleryMarkers.addEventListener('mouseup', logMouseGalleryMarkers);
-zooMarkers.addEventListener('mouseup', logMouseZooMarkers);
-stadiumMarkers.addEventListener('mouseup', logMouseStadiumMarkers);
-busMarkers.addEventListener('mouseup', logMouseBusMarkers);
-subwayMarkers.addEventListener('mouseup', logMouseSubwayMarkers);
-taxiMarkers.addEventListener('mouseup', logMouseTaxiMarkers);
-airportMarkers.addEventListener('mouseup', logMouseAirportMarkers);
 
-function logMouseHotelMarkers(e) {
-  if (typeof e === 'object') {
-    switch (e.button) {
-    case 0:
-        onClickClusterButton ='lodging';
-        alert('Button clicked ID: '+ this.id + ' Value: '+ onClickClusterButton);
-        break;
-    default:
-        alert = `Unknown button code: ${btnCode}`;
-    }
-  }
-}
-function logMouseFoodMarkers(e) {
-  if (typeof e === 'object') {
-    switch (e.button) {
-    case 0:
-        onClickClusterButton ='restaurant';
-        alert('Button clicked ID: '+ this.id + ' Value: '+ onClickClusterButton);;
-        break;
-    default:
-        alert = `Unknown button code: ${btnCode}`;
-    }
-  }
-}
+// hotelMarkers.addEventListener('mouseup', logMouseHotelMarkers);
+// foodMarkers.addEventListener('mouseup', logMouseFoodMarkers);
+// pubMarkers.addEventListener('mouseup', logMousePubMarkers);
+// atmMarkers.addEventListener('mouseup', logMouseAtmMarkers);
+// museumMarkers.addEventListener('mouseup', logMouseMuseumMarkers);
+// galleryMarkers.addEventListener('mouseup', logMouseGalleryMarkers);
+// zooMarkers.addEventListener('mouseup', logMouseZooMarkers);
+// stadiumMarkers.addEventListener('mouseup', logMouseStadiumMarkers);
+// busMarkers.addEventListener('mouseup', logMouseBusMarkers);
+// subwayMarkers.addEventListener('mouseup', logMouseSubwayMarkers);
+// taxiMarkers.addEventListener('mouseup', logMouseTaxiMarkers);
+// airportMarkers.addEventListener('mouseup', logMouseAirportMarkers);
+
+// function logMouseHotelMarkers(e) {
+//   if (typeof e === 'object') {
+//     switch (e.button) {
+//     case 0:
+//         onClickClusterButton ='lodging';
+//         // alert('Button clicked ID: '+ this.id + ' Value: '+ onClickClusterButton);
+//         // test onClickClusterButton value change
+//         document.getElementById('map-user-selections').innerHTML = onClickClusterButton;
+
+
+//         break;
+//     default:
+//         alert = `Unknown button code: ${btnCode}`;
+//     }
+//   }
+//   else onClickClusterButton =''
+// };
+// function logMouseFoodMarkers(e) {
+//   if (typeof e === 'object') {
+//     switch (e.button) {
+//     case 0:
+//         onClickClusterButton ='restaurant';
+//         alert('Button clicked ID: '+ this.id + ' Value: '+ onClickClusterButton);;
+//         break;
+//     default:
+//         alert = `Unknown button code: ${btnCode}`;
+//     }
+   
+//   }
+//   else onClickClusterButton =''
+// };
+
 // document.getElementById('hotel-markers').onclick = function() {
 // alert('Button clicked ID: '+ this.id + ' Value: '+ onClickClusterButton );}
 // document.getElementById('food-markers').onclick = function() {
